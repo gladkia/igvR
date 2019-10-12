23a1c7bb62d5d6317f366b16ea9a5148c00b4663
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-10-12 13:21:49 -0700

    removed old extracted R code

---
+++ vignettes/AlzheimersVariantsNearMEF2C.R
@@ -0,0 +1,124 @@
+## ----setup, include = FALSE----------------------------------------------
+options(width=120)
+knitr::opts_chunk$set(
+   collapse = TRUE,
+   eval=interactive(),
+   echo=TRUE,
+   comment = "#>"
+)
+
+
+## ---- eval=TRUE, echo=FALSE----------------------------------------------
+knitr::include_graphics("igvR-mef2c-Alzheimers-demo.png")
+
+
+# Load the libraries we need
+
+
+## ----loadLibraries,  results='hide'--------------------------------------
+library(igvR)
+library(MotifDb)
+library(BSgenome.Hsapiens.UCSC.hg19)
+library(VariantAnnotation)
+library(AnnotationHub)
+
+
+## ----createLoad, results='hide'------------------------------------------
+igv <- igvR()
+setBrowserWindowTitle(igv, "MEF2C")
+setGenome(igv, "hg19")
+
+
+## ----showMEF2C,  results='hide'------------------------------------------
+showGenomicRegion(igv, "MEF2C")
+
+
+## ----roi,  results='hide'------------------------------------------------
+chrom <- "chr5"
+shoulder <- 50000
+start.loc <- 88013975 - shoulder
+end.loc   <- 88199922 + shoulder
+mef2c.region <- GRanges(seqnames=chrom, IRanges(start=start.loc, end=end.loc))
+showGenomicRegion(igv, list(chrom=chrom, start=start.loc, end=end.loc))
+
+
+## ----data.frame.track,  results='hide'-----------------------------------
+load(system.file(package="igvR", "extdata", "tbl.mef2cGWAS.variants.RData"))
+tbl.mef2cGWAS.variants.bed <- tbl.mef2cGWAS.variants[, c("CHR", "oldPos", "oldPos", "SNP", "P")]
+tbl.mef2cGWAS.variants.bed$P <- -log10(tbl.mef2cGWAS.variants.bed$P)
+colnames(tbl.mef2cGWAS.variants.bed) <- c("chrom", "start", "end", "name", "score")
+track.gwas <- DataFrameAnnotationTrack("IGAP.gwas", tbl.mef2cGWAS.variants.bed, trackHeight=20, color="darkBlue")
+displayTrack(igv, track.gwas)
+
+tbl.mef2cGWAS.variants.bedGraph <-  tbl.mef2cGWAS.variants.bed[, -4]
+track.gwas.numeric <- DataFrameQuantitativeTrack("IGAP.gwas.scored", tbl.mef2cGWAS.variants.bedGraph, autoscale=TRUE)
+displayTrack(igv, track.gwas.numeric)
+
+
+
+## ----queryAHforPromoters,  results='hide'--------------------------------
+ah <- AnnotationHub()
+ah.human <- subset(ah, species == "Homo sapiens")
+#----------------------------------------------------------------------------------------------------
+# add refseq promoters, available from RefSeq for each transcript which has been identified
+#----------------------------------------------------------------------------------------------------
+ah.human.refseq <- query(ah.human, "RefSeq", "hg19", "RefSeq Genes")
+
+# download the first set
+human.refseq <- ah.human.refseq[[1]]
+gr.promoters <- promoters(human.refseq, upstream=2000, downstream=200)
+  # get rid of score, itemRgb, thick, blocks columns in the mcols, keeping just the transcript name.
+  # these attributes are meaningful for transcript tracks since those include the represenation
+  # of UTRs, introns and exons.   but a promoter is a stretch of DNA for which those distinctions
+  # do not apply
+mcols(gr.promoters) <- mcols(gr.promoters)[,1]
+colnames(mcols(gr.promoters)) <- "name"
+ov <- findOverlaps(gr.promoters, mef2c.region)
+gr.mef2c.promoters <- gr.promoters[queryHits(ov)]
+track.promoters <- UCSCBedAnnotationTrack("promoters", gr.mef2c.promoters, color="darkGreen")
+displayTrack(igv, track.promoters)
+
+
+## ----overlapPromotersAndVariants,  results='hide'------------------------
+gr.variants <- GRanges(tbl.mef2cGWAS.variants.bed)
+ov <- findOverlaps(gr.variants, gr.promoters)
+gr.variantsInPromoters <- gr.variants[queryHits(ov)]
+track.variantsInPromoters <-GRangesAnnotationTrack("snpInPromoters", gr.variantsInPromoters,
+                                                   color="red", displayMode="EXPANDED")
+displayTrack(igv, track.variantsInPromoters)
+
+
+## ----methylationTracks,  results='hide'----------------------------------
+histone.tracks <- query(ah.human, c("H3K4me3", "Gm12878", "Peak", "narrow"))  # 3 tracks
+descriptions <- histone.tracks$description
+titles <- histone.tracks$title
+colors <- rep(terrain.colors(6), 4)
+color.index <- 0
+
+for(i in seq_len(length(histone.tracks))){
+   name <- names(histone.tracks)[i]
+   color.index <- color.index + 1
+   gr <- histone.tracks[[name]]
+   ov <- findOverlaps(gr, mef2c.region)
+   mef2c.histones <- gr[queryHits(ov)]
+   track.histones <- GRangesQuantitativeTrack(titles[i], mef2c.histones[, "pValue"],
+                                              color=colors[color.index], trackHeight=50,
+                                              autoscale=TRUE)
+   displayTrack(igv, track.histones)
+   Sys.sleep(5)
+   } # for track
+
+
+
+## ----adniVCF,  results='hide'--------------------------------------------
+vcfFilename <- system.file(package="igvR", "extdata", "mef2c-4kb.vcf")
+vcf <- readVcf(vcfFilename, "hg19")
+track.vcf <- VariantTrack("AMPAD VCF", vcf, trackHeight=1000)
+Sys.sleep(5)
+displayTrack(igv, track.vcf)
+Sys.sleep(10)  # allow plenty of time for this to complete, and the track to appear, before ending this chunk
+
+
+## ----sessionInfo---------------------------------------------------------
+sessionInfo()
+
--- vignettes/ctcfChipSeq.R
+++ vignettes/ctcfChipSeq.R
@@ -1,16 +1,36 @@
+## ----setup, include = FALSE----------------------------------------------
+options(width=120)
+knitr::opts_chunk$set(
+   collapse = TRUE,
+   eval=interactive(),
+   echo=TRUE,
+   comment = "#>"
+)
+
+
+## ---- eval=TRUE, echo=FALSE----------------------------------------------
+knitr::include_graphics("ctcf-chip-seq-gata2-252kb-igvR.png")
+
+
+## ----loadLibraries,  results='hide'--------------------------------------
 library(igvR)
 library(MotifDb)
 library(BSgenome.Hsapiens.UCSC.hg19)
 
+
+## ----createLoad, results='hide'------------------------------------------
 igv <- igvR()
-
+setBrowserWindowTitle(igv, "CTCF ChIP-seq")
 setGenome(igv, "hg19")
-showGenomicRegion(igv, "GATA2")
+
+
+## ----initialDisplay,  results='hide'-------------------------------------
 showGenomicRegion(igv, "chr3:128,079,020-128,331,275")
+
+
+## ----roi,  results='hide'------------------------------------------------
 loc <- getGenomicRegion(igv)
-
 which <- with(loc, GRanges(seqnames=chrom, ranges = IRanges(start, end)))
-
 param <- ScanBamParam(which=which, what = scanBamWhat())
 bamFile <- "~/github/ChIPseqMotifMatch/bulk/GSM749704/GSM749704_hg19_wgEncodeUwTfbsGm12878CtcfStdAlnRep1.bam"
 file.exists(bamFile)
@@ -18,8 +38,10 @@ x <- readGAlignments(bamFile, use.names=TRUE, param=param)
 track <- GenomicAlignmentTrack("ctcf bam", x, visibilityWindow=10000000, trackHeight=200)  # 30000 default
 displayTrack(igv, track)
 
-tbl.pk <- read.table("macs2/GSM749704_hg19_chr19_peaks.narrowPeak", sep="\t",
-                     header=FALSE, as.is=TRUE)[, c(1,2,3,5)]
+
+## ----narrow.peaks.track,  results='hide'---------------------------------
+filename <- system.file(package="igvR", "extdata", "GSM749704_hg19_chr19_peaks.narrowPeak")
+tbl.pk <- read.table(filename, sep="\t", header=FALSE, as.is=TRUE)[, c(1,2,3,5)]
 dim(tbl.pk)
 colnames(tbl.pk) <- c("chrom", "start", "end", "score")
 loc <- getGenomicRegion(igv)
@@ -35,23 +57,15 @@ dim(tbl.pk)
 track <- DataFrameQuantitativeTrack("macs2 peaks", tbl.pk, color="red", autoscale=TRUE)
 displayTrack(igv, track)
 
-enableMotifLogoPopups(igv, TRUE)
-pfm.ctcf <- query(MotifDb, c("CTCF", "sapiens", "jaspar2018"), notStrings="ctcfl")
-motif.name <- names(pfm.ctcf)
-pfm <- pfm.ctcf[[1]]
 
-
-
-loc <- getGenomicRegion(igv)
-
-while(!is.list(loc)){
-   print(loc)
-   loc <- getGenomicRegion(igv)
-   }
-printf("--- good loc obtained? ")
-print(loc)
+## ----motif.match.and.display, results='hide'-----------------------------
 
 dna <- with(loc, getSeq(BSgenome.Hsapiens.UCSC.hg19, chrom, start, end))
+
+
+pfm.ctcf <- query(MotifDb, c("CTCF", "sapiens", "jaspar2018"), notStrings="ctcfl")
+motif.name <- names(pfm.ctcf)[1]
+pfm <- pfm.ctcf[[1]]
 
 hits.forward <- matchPWM(pfm, as.character(dna), with.score=TRUE, min.score="80%")
 hits.reverse <- matchPWM(reverseComplement(pfm), as.character(dna), with.score=TRUE, min.score="80%")
@@ -68,6 +82,9 @@ tbl.matches$chrom <- loc$chrom
 tbl.matches$start <- tbl.matches$start + loc$start
 tbl.matches$end <- tbl.matches$end + loc$start
 with(tbl.matches, end-start)
+
+enableMotifLogoPopups(igv)
+
 tbl.matches$name <- paste0("MotifDb::", motif.name)
 tbl.matches <- tbl.matches[, c("chrom", "start", "end", "name", "score")]
 dim(tbl.matches)
@@ -78,3 +95,9 @@ displayTrack(igv, track)
 
 track <- DataFrameQuantitativeTrack("MA0139.1 scores", tbl.matches[, c(1,2,3,5)], color="random", autoscale=FALSE, min=8, max=12)
 displayTrack(igv, track)
+
+
+
+## ----sessionInfo---------------------------------------------------------
+sessionInfo()
+
