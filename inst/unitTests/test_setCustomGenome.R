aaf3c36c0216bdd4fe32832b721b18a5d4c02086
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-11-01 07:07:21 -0800

    added test/demo for setCustomGenome.R

--- inst/unitTests/test_setCustomGenome.R
+++ inst/unitTests/test_setCustomGenome.R
@@ -8,24 +8,23 @@ checkTrue(ready(igv))
 allArgs <- FALSE
 if(allArgs){
     setCustomGenome(igv,
-                id="hg38",
-                genomeName="Human (GRCh38/hg38)",
-                fastaURL="https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg38/hg38.fa",
-                fastaIndexURL="https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg38/hg38.fa.fai",
-                cytobandURL="https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg38/cytoBandIdeo.txt",
-                chromosomeAliasURL=NA,
-                geneAnnotationName="Refseq Genes",
-                geneAnnotationURL="https://s3.amazonaws.com/igv.org.genomes/hg38/refGene.txt.gz",
-                geneAnnotationTrackHeight=500,
-                initialLocus="chr5:88,621,308-89,001,037",
-                visibilityWindow=5000000)
+                    id="hg38",
+                    genomeName="Human (GRCh38/hg38)",
+                    fastaURL="https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg38/hg38.fa",
+                    fastaIndexURL="https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg38/hg38.fa.fai",
+                    cytobandURL="https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg38/cytoBandIdeo.txt",
+                    chromosomeAliasURL=NA,
+                    geneAnnotationName="Refseq Genes",
+                    geneAnnotationURL="https://s3.amazonaws.com/igv.org.genomes/hg38/refGene.txt.gz",
+                    geneAnnotationTrackHeight=500,
+                    geneAnnotationTrackColor="red",
+                    initialLocus="chr5:88,621,308-89,001,037",
+                    visibilityWindow=5000000)
 
    } else {
     setCustomGenome(igv,
-                id="hg38",
-                genomeName="Human (GRCh38/hg38)",
-                fastaURL="https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg38/hg38.fa",
-                fastaIndexURL="https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg38/hg38.fa.fai")
-                #geneAnnotationName="Refseq Genes",
-                #geneAnnotationURL="https://s3.amazonaws.com/igv.org.genomes/hg38/refGene.txt.gz")
+                    id="hg38",
+                    genomeName="Human (GRCh38/hg38)",
+                    fastaURL="https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg38/hg38.fa",
+                    fastaIndexURL="https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg38/hg38.fa.fai")
     }
