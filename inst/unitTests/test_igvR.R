63f2f66cfd2ababb81674bc99f17f7896b5f5ac6
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-07-17 17:37:01 -0700

    added test (actually a demo) of new RemoteAlignmentTrack in action

--- inst/unitTests/test_igvR.R
+++ inst/unitTests/test_igvR.R
@@ -624,6 +624,21 @@ test_displayAlignment <- function()
 
 } # test_displayAlignment
 #------------------------------------------------------------------------------------------------------------------------
+test_displayRemoteAlignment <- function()
+{
+   message(sprintf("--- test_displayRemoteAlignment"))
+
+   setGenome(igv, "hg19")
+   showGenomicRegion(igv, "GATA2")
+
+   url <- "https://1000genomes.s3.amazonaws.com/phase3/data/HG02450/alignment/HG02450.mapped.ILLUMINA.bwa.ACB.low_coverage.20120522.bam"
+   index <- "https://1000genomes.s3.amazonaws.com/phase3/data/HG02450/alignment/HG02450.mapped.ILLUMINA.bwa.ACB.low_coverage.20120522.bam.bai"
+
+   track <- RemoteAlignmentTrack("Alignment", url, index, visibilityWindow=100000)
+   displayTrack(igv, track)
+
+} # test_displayRemoteAlignment
+#------------------------------------------------------------------------------------------------------------------------
 test_displayBedpeInteractions <- function()
 {
    message(sprintf("--- test_displayBedpeInteractions"))
