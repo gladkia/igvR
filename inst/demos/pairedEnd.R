b011f3beaff8169192d604874845516c599ea857
paul-shannon <paul.thurmond.shannon@gmail.com>
2023-03-07 11:06:12 -0800

    single-base anchors demo'd

--- inst/demos/pairedEnd.R
+++ inst/demos/pairedEnd.R
@@ -21,3 +21,11 @@ roi <- sprintf("%s:%d-%d", tbl.bedpe$chrom1[1],
 showGenomicRegion(igv, roi)
 track <- BedpeInteractionsTrack("ENCFF110BUX", tbl.bedpe)
 displayTrack(igv, track)
+
+  # TADs can come in with single base anchors.  make that work
+
+tbl.bedpe2 <- tbl.bedpe[, c("chrom1", "start1", "start1", "chrom2", "start2", "start2")]
+colnames(tbl.bedpe2)[c(3,5)] <- c("end1", "end2")
+track <- BedpeInteractionsTrack("single-base anchors", tbl.bedpe2)
+displayTrack(igv, track)
+
